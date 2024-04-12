dec23732a4c0ad714d6cdc20a4e1eaddef35c20f
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-04-12 15:08:52 +0200

    mo process executer to a different module (#10)

--- Program.fs
+++ Program.fs
@@ -1,39 +1,13 @@
-﻿module EcoSystem.Scripts
-
+﻿namespace WorkingEcosystem
 open System.IO
-open System
 
-type ProcessResult = { 
-    ExitCode : int; 
-    StdOut : string; 
-    StdErr : string 
-}
-
-let executeProcess (processName: string) (processArgs: string) =
-    let psi = new Diagnostics.ProcessStartInfo(processName, processArgs) 
-    psi.UseShellExecute <- false
-    psi.RedirectStandardOutput <- true
-    psi.RedirectStandardError <- true
-    psi.CreateNoWindow <- true        
-    let proc = Diagnostics.Process.Start(psi) 
-    let output = new Text.StringBuilder()
-    let error = new Text.StringBuilder()
-    proc.OutputDataReceived.Add(fun args -> output.Append(args.Data) |> ignore)
-    proc.ErrorDataReceived.Add(fun args -> error.Append(args.Data) |> ignore)
-    proc.BeginErrorReadLine()
-    proc.BeginOutputReadLine()
-    proc.WaitForExit()
-    { ExitCode = proc.ExitCode; StdOut = output.ToString(); StdErr = error.ToString() }
-
-
-let outcomeDirectory = Directory.CreateDirectory("outcome")
-let fendDirectory = Directory.CreateDirectory("outcome/fend")
-let bendDirectory = Directory.CreateDirectory("outcome/bend")
-let callsDirectory = Directory.CreateDirectory("outcome/calls")
-
-printfn "directories created, copy the content of outcome ..."
-
-
-let openFend = executeProcess "explorer" "C:\\feature namings\\toli\\cuda\\material selection\\fend"
-let openBend = executeProcess "explorer" "C:\\feature namings\\toli\\cuda\\material selection\\bend"
-let openCalls = executeProcess "explorer" "C:\\feature namings\\toli\\cuda\\material selection\\calls"
+module main =
+    let outcomeDirectory = Directory.CreateDirectory("outcome")
+    let fendDirectory = Directory.CreateDirectory("outcome/fend")
+    let bendDirectory = Directory.CreateDirectory("outcome/bend")
+    let callsDirectory = Directory.CreateDirectory("outcome/calls")
+    printfn "directories created, copy the content of outcome ..."
+    open Utils
+    let openFend = executeProcess "explorer" "C:\\feature namings\\toli\\cuda\\material selection\\fend"
+    let openBend = executeProcess "explorer" "C:\\feature namings\\toli\\cuda\\material selection\\bend"
+    let openCalls = executeProcess "explorer" "C:\\feature namings\\toli\\cuda\\material selection\\calls"
